90c9957d9e70982380d1f0916485231d250abe25
JVThomas <jodyvthomas@gmail.com>
2024-01-15 14:27:10 +0200

    to mac

---
+++ app/app/calc.py
@@ -0,0 +1,23 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
+
+
+def multiply(x, y):
+    """Multiply x and y and return result."""
+    return x * y
+
+
+def divide(x, y):
+    """Divide x by y and return result."""
+    return x / y
